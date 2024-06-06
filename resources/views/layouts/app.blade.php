58cafa303bfd3ef3e7880e75a3f948912e608fd0
Kangmieboyolali <lukmannur90@gmail.com>
2024-06-06 22:33:48 +0700

    update navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,11 +24,14 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                        <button type="button" class="btn btn-link nav-link" onclick="window.location.href='{{ route('home') }}'">Home</button>
+                    </li>
                     <li class="nav-item">
                         <button type="button" class="btn btn-link nav-link" onclick="window.location.href='{{ route('registrations.index') }}'">Rekap Peserta</button>
                     </li>
                     <li class="nav-item">
-                        <button type="button" class="btn btn-link nav-link" onclick="window.location.href='{{ route('seminar.rekap') }}'">Rekap Seminar</button>
+                        <button type="button" class="btn btn-link nav-link" onclick="window.location.href='{{ route('home') }}'">Rekap Seminar</button>
                     </li>
                     <li class="nav-item">
                         <button type="button" class="btn btn-link nav-link" onclick="window.location.href='{{ route('seminars.rekap-peserta') }}'">Data User</button>
