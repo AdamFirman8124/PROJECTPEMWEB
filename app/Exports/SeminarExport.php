e4d245be95beffa70db362719a6316852e162417
edokarnaedi <karnaediedo@gmail.com>
2024-06-21 08:48:36 +0700

    menambahkan tabel pada excel seminar materi

--- app/Exports/SeminarExport.php
+++ app/Exports/SeminarExport.php
@@ -5,9 +5,11 @@ namespace App\Exports;
 use App\Models\Seminar;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use App\Models\Materi;
 
-class SeminarExport implements FromCollection, WithHeadings
+class SeminarExport implements FromCollection, WithHeadings, WithStyles
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -24,5 +26,34 @@ class SeminarExport implements FromCollection, WithHeadings
     {
         return ['ID', 'Judul Materi', 'Dibuat Pada', 'Diperbarui Pada'];
     }
-}
 
+    /**
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        // Apply styles to the entire sheet
+        $sheet->getStyle('A1:D1')->applyFromArray([
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+            'font' => [
+                'bold' => true,
+            ],
+        ]);
+
+        // Apply borders to the rest of the rows
+        $sheet->getStyle('A2:D' . $sheet->getHighestRow())->applyFromArray([
+            'borders' => [
+                'allBorders' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+                ],
+            ],
+        ]);
+
+        return [];
+    }
+}
