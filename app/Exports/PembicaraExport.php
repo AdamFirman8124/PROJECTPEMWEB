c9d302edffae5c6dc3a419d05ef14df1e5e59ad0
rubyry <ryadishanifah@gmail.com>
2024-07-03 01:37:35 +0700

    update export pembicara

--- app/Exports/PembicaraExport.php
+++ app/Exports/PembicaraExport.php
@@ -4,16 +4,19 @@ namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithColumnWidths;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use App\Models\Pembicara;
 
-class PembicaraExport implements FromCollection, WithHeadings
+class PembicaraExport implements FromCollection, WithHeadings, WithColumnWidths, WithStyles
 {
     /**
      * @return \Illuminate\Support\Collection
      */
     public function collection()
     {
-        return Pembicara::all();
+        return Pembicara::select('id', 'nama_pembicara', 'topik', 'asal_instansi')->get();
     }
 
     /**
@@ -26,6 +29,40 @@ class PembicaraExport implements FromCollection, WithHeadings
             'Nama Pembicara',
             'Topik',
             'Asal Instansi'
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function columnWidths(): array
+    {
+        return [
+            'A' => 5,
+            'B' => 30,
+            'C' => 50,
+            'D' => 30
+        ];
+    }
+
+    /**
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Heading style
+            1 => [
+                'font' => [
+                    'bold' => true,
+                    'color' => ['argb' => 'FFFFFFFF'],
+                ],
+                'fill' => [
+                    'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                    'startColor' => ['argb' => 'A675E4']
+                ]
+            ],
         ];
     }
 }
